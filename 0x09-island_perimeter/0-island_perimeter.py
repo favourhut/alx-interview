ba2698b480918d609f76ba8c1d1b0c13ec1f531d
Favour James <Favourjames061@gmail.com>
2024-12-04 00:38:22 +0000

    Fix white space

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -20,7 +20,7 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 size += 1
                 if (j > 0 and grid[i][j - 1] == 1):
-                    sides +=1
+                    sides += 1
                 if (i > 0 and grid[i - 1][j] == 1):
                     sides +=1
     return size * 4 - sides * 2
